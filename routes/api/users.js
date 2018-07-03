ce944b37ca81426ad56775d49f82165298753a0e
unknown <guilherme.atu@gmail.com>
2018-07-03 16:15:19 -0300

    Email & Password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,30 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @acess   Public
+//---------------------------------//
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for users
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Sucess" });
+      } else {
+        return res.status(400).json({ password: "Password incorret" });
+      }
+    });
+  });
+});
+
 module.exports = router;
