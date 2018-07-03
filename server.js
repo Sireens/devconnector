c9302417b49342d37e9a479e84f496f386080c8b
unknown <guilherme.atu@gmail.com>
2018-07-03 15:48:15 -0300

    User registration & postman tests

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-// DB Config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// DB Config
 //const db = require("./config/keys").mongoURI;
 const db = process.env.mongoURI;
 
@@ -18,6 +26,11 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Helllllo");
 });
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
