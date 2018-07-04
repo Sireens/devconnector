f1996d14c70b492ec4842e8751aeafcc599d6a84
Guilherme Oliveira <guilherme.atu@gmail.com>
2018-07-03 22:16:33 -0300

    Validation Handlers - 1

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 require('dotenv').config();
-//const db = process.env.mongoURI;
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
 
 // Load User model
 const User = require('../../models/User');
@@ -21,9 +23,17 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @acess   Public
 //---------------------------------//
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //Size
