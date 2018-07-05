61582ac69b5d94c0e6c3e88926669061d82f3694
Guilherme Oliveira <guilherme.atu@gmail.com>
2018-07-04 22:26:48 -0300

    Validation Handlers - 2

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -66,6 +67,12 @@ router.post('/register', (req, res) => {
 // @acess   Public
 //---------------------------------//
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
   const password = req.body.password;
 
@@ -73,7 +80,8 @@ router.post('/login', (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for users
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -96,7 +104,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: 'Password incorret' });
+        errors.password = 'Password incorret';
+        return res.status(400).json(errors);
       }
     });
   });
